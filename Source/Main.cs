2b4580cbd0f8d92f5bc119b7c522609d2f46258a
John W. Leimgruber III <leimgrub@gmail.com>
2020-11-10 17:35:03 -0500

    skeleton project compiles now

---
+++ Source/Main.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using RimWorld;
+using System;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace KanbanStockpile
+{
+    [StaticConstructorOnStartup]
+    public static class KanbanStockpileLoader
+    {
+        public static bool IsSameSpotInstalled;
+
+        static KanbanStockpileLoader()
+        {
+            var harmony = new Harmony("net.ubergarm.rimworld.mods.kanbanstockpile");
+            harmony.PatchAll();
+
+            if (MP.enabled) {
+                MP.RegisterAll();
+            }
+        }
+    }
+
+	public class KanbanStockpile : Mod
+	{
+        public KanbanStockpile(ModContentPack content) : base(content)
+        {
+        }
+    }
+
+}
